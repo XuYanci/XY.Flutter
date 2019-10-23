b9fef53180884cc30bd5ed7746b2290520c7e82d
Wind Randy <grandy.wind@gmail.com>
2019-10-23 18:02:25 +0800

    commit some

--- state_management/InheritWidget/app/lib/main.dart
+++ state_management/InheritWidget/app/lib/main.dart
@@ -36,8 +36,8 @@ class ShareDataWidget extends InheritedWidget {
 
   //定义一个便捷方法，方便子树中的widget获取共享数据  
   static ShareDataWidget of(BuildContext context) {
-    // return context.inheritFromWidgetOfExactType(ShareDataWidget);
-   return context.ancestorInheritedElementForWidgetOfExactType(ShareDataWidget).widget;
+    return context.inheritFromWidgetOfExactType(ShareDataWidget);
+  //  return context.ancestorInheritedElementForWidgetOfExactType(ShareDataWidget).widget;
   }
 
   //该回调决定当data发生变化时，是否通知子树中依赖data的Widget  
